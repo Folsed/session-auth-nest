11ac902c074be4aed560ec8b1ad35464cfa4ebe7
Kyrylo <dexter37814@gmail.com>
2025-06-02 19:49:17 +0300

    feat(database): add typeorm configuration

---
+++ src/database/database.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+    imports: [
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: async (configService: ConfigService) => ({
+                type: 'postgres',
+                host: configService.get<string>('DB_HOST'),
+                port: configService.get<number>('DB_PORT'),
+                username: configService.get<string>('DB_USERNAME'),
+                password: configService.get<string>('DB_PASSWORD'),
+                database: configService.get<string>('DB_NAME'),
+                entities: [__dirname + '/../**/*.entity.{ts,js}'],
+                migrations: [__dirname + '/migrations/*{.ts,.js}'],
+                autoLoadEntities: true,
+                synchronize: true,
+                logging: configService.get<boolean>('DB_LOGGING')
+            })
+        })
+    ]
+})
+export class DatabaseModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+    const app = await NestFactory.create(AppModule);
+
+    app.setGlobalPrefix('api');
+
+    await app.listen(4000);
 }
 
 bootstrap();
